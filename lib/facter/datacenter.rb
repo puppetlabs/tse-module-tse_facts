355443cf1b518f5e600bcad10322d10866f46915
Mike Smith <mike.smith@puppet.com>
2016-12-21 07:38:46 -0800

    Update datacenter.rb

--- lib/facter/datacenter.rb
+++ lib/facter/datacenter.rb
@@ -1,6 +1,22 @@
+begin
+  require 'facter/util/puppet_settings'
+rescue LoadError => e
+  # puppet apply does not add module lib directories to the $LOAD_PATH (See
+  # #4248). It should (in the future) but for the time being we need to be
+  # defensive which is what this rescue block is doing.
+  rb_file = File.join(File.dirname(__FILE__), 'util', 'puppet_settings.rb')
+  load rb_file if File.exists?(rb_file) or raise e
+end
+
 Facter.add('datacenter') do
   setcode do
-    domain = Facter.value('domain')
+    if (domain = Facter.value('domain')).nil?
+      #if domain is null, then fall back to certname
+      domain = Facter::Util::PuppetSettings.with_puppet do
+        Puppet[:certname].split('.')[1..-1].join('.')
+      end
+    end
+    
     case domain
     when 'inf.puppet.vm' 
       'infrastructure'
@@ -9,7 +25,7 @@ Facter.add('datacenter') do
     when 'pdx.puppet.vm'
       'portland'
     else
-      'unknown'  # fall back to cert name here?
+      'unknown'
     end
   end
 end
