20a6201a75890d7d7613d7af7f7c62829f3dac06
Mike Smith <mike.smith@puppet.com>
2017-10-20 08:42:17 -0700

    Update windowsdomain.rb

--- lib/facter/windowsdomain.rb
+++ lib/facter/windowsdomain.rb
@@ -1,10 +1,10 @@
-Facter.add('domain') do
+Facter.add('domain2') do
   confine :osfamily => :windows
   setcode do
     begin
       require 'win32ole'
       wmi = WIN32OLE.connect("winmgmts:\\\\.\\root\\cimv2")
-      domain = wmi.ExecQuery("SELECT * FROM Win32_ComputerSystem.domain")
+      domain = wmi.ExecQuery("SELECT * FROM Win32_ComputerSystem.domain").each.first
       domain
     rescue
     end
