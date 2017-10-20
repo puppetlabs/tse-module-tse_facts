c47dcb15a35267295f3d8f14a4bf55bcb2867853
Mike Smith <mike.smith@puppet.com>
2017-10-20 08:32:55 -0700

    Update windows_domain.ps1

--- facts.d/windows_domain.ps1
+++ facts.d/windows_domain.ps1
@@ -1,4 +1,2 @@
-$domain_member = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
 $domain = (Get-WmiObject Win32_ComputerSystem).Domain
-write-host "domain_member=$($domain_member)"
 write-host "domain=$($domain)"
