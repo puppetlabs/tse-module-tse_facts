1fba35af56b46e095038b4682ff1432a6ff4c849
Mike Smith <mike.smith@puppet.com>
2017-10-20 09:07:35 -0700

    Delete windows_domain.ps1

--- facts.d/windows_domain.ps1
+++
@@ -1,4 +0,0 @@
-$domain = (Get-WmiObject Win32_ComputerSystem).Domain
-$domain_member = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
-write-host "domain_member=$($domain_member)"
-write-host "domain=$($domain)"
