7f3171bc9152bf671ede4fa1ecc5b4f537ba0418
Mike Smith <mike.smith@puppet.com>
2017-10-31 08:48:23 -0700

    Update windows_services.ps1

--- facts.d/windows_services.ps1
+++ facts.d/windows_services.ps1
@@ -1,7 +1,5 @@
 #$var = (Get-Service | Where-Object {$_.status -eq "running"} | Select-Object Name, Status | ConvertTo-Json -Depth 1)
 #write-host "Windows_Services="$var
 
-$var = (Get-Service | Where-Object {$_.status -eq "running"} | Select-Object Name, Status) | ConvertTo-Json -Depth 1
-write-host "{"windows_services":$($var)}"
-
-mkdir -P c:\testing
+$var = @{"Windows_Services"= (Get-Service | Where-Object {$_.status -eq "running"} | Select-Object Name, Status)} | ConvertTo-Json -Compress -Depth 1
+write-host $var
