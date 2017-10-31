e1e5f582a438ae0c7d4914623eece008b935b364
Mike Smith <mike.smith@puppet.com>
2017-10-31 10:03:51 -0700

    Delete windows_services.ps1

--- facts.d/windows_services.ps1
+++
@@ -1,4 +0,0 @@
-#$var = (Get-Service | Where-Object {$_.status -eq "running"} | Select-Object Name, Status | ConvertTo-Json -Depth 1)
-#write-host "Windows_Services="$var
-$var = @{"Windows_Services"= (Get-Service | Where-Object {$_.status -eq "running"} | Select-Object Name, Status) | convertto-json -compress -Depth 1} | ConvertTo-Json -compress -Depth 1
-write-host $var
